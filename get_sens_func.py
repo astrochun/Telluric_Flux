8b49ecf4957cbbc7d6b24dfeb8ee0d1cb604c299
Chun Ly <astro.chun@gmail.com>
2018-04-18 20:44:03 -0700

    get_sens_func.main: Get filter center and width from spec_convolve(); Plot photometry-normalized F_nu vs lambda

--- get_sens_func.py
+++ get_sens_func.py
@@ -17,6 +17,8 @@ from astropy import log
 
 from . import read_stellar_template, spec_convolve, get_photometry
 
+c_Ang = spec_convolve.c_Ang
+
 def main(name, library='Pickles'):
 
     '''
@@ -33,21 +35,31 @@ def main(name, library='Pickles'):
     Notes
     -----
     Created by Chun Ly, 18 April 2018
+     - Get filter center and width from spec_convolve()
+     - Plot photometry-normalized F_nu vs lambda
     '''
     
     log.info('### Begin main ! ')
 
     phot0, err_phot0, sptype, tab0 = get_photometry.main(name)
-    mag0 = [phot0['J'], phot0['H'], phot0['K']]
-    
+    mag0 = np.array([phot0['J'], phot0['H'], phot0['K']])
+
     wave, F_nu = read_stellar_template.main(sptype, library=library)
 
-    model_mag0 = spec_convolve.main(wave, F_nu, AB=False)
+    model_mag0, wave_cen0, FWHM0 = spec_convolve.main(wave, F_nu, AB=False)
+    print wave_cen0, FWHM0
 
     mag_diff     = mag0 - model_mag0
     avg_mag_diff = np.average(mag_diff)
 
     F_nu /= 10**(avg_mag_diff/2.5)
+
+    F_lam = F_nu * c_Ang / (wave**2)
+
+    plt.semilogy(wave, F_nu)
+    #plt.errorbar(wave_cen0, 10**(-0.4*mag0), xerr=FWHM0/2.0, marker='o',
+    #             ecolor='black', fmt=None, mec='blue')
+    #plt.ylim([1e-15,1e-11])
 
     log.info('### End main ! ')
 #enddef
